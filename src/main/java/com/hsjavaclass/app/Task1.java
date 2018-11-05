c49fc40d834019aff63a5148afed8d0626e05956
Shannon B <shanbering@gmail.com>
2018-11-05 21:01:54 +0100

    :sparkles: homework 1

---
+++ src/main/java/com/hsjavaclass/app/Task1.java
@@ -0,0 +1,14 @@
+package com.hsjavaclass.app;
+
+public class Task1 {
+        
+    private static int give_me_one() {
+        return 1;
+    }
+    
+    public static void Main(String[] args) {
+        int one = give_me_one();
+        int two = one * 2;
+        System.out.println(two);
+    }
+}
