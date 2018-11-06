f80812a814df19bb1fb40dd764b757ffb93ac4ae
Shannon B <shanbering@gmail.com>
2018-11-06 20:08:40 +0100

    :sparkles: changed to memory measuring task

--- src/main/java/com/hsjavaclass/app/App.java
+++ src/main/java/com/hsjavaclass/app/App.java
@@ -8,6 +8,36 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            System.gc();
+            Thread.sleep(10);
+            long before = Runtime.getRuntime().freeMemory();
+            int num = 1;
+            long after = Runtime.getRuntime().freeMemory();
+            System.out.println(before - after);
+
+            System.gc();
+            Thread.sleep(10);
+            before = Runtime.getRuntime().freeMemory();
+            int[] array = new int[5];
+            after = Runtime.getRuntime().freeMemory();
+            System.out.println(before - after);
+
+            System.gc();
+            Thread.sleep(10);
+            before = Runtime.getRuntime().freeMemory();
+            Object thing = new Object();
+            after = Runtime.getRuntime().freeMemory();
+            System.out.println(before - after);
+
+            System.gc();
+            Thread.sleep(10);
+            before = Runtime.getRuntime().freeMemory();
+            String word = "hello hgf";
+            after = Runtime.getRuntime().freeMemory();
+            System.out.println(before - after);
+        } catch(Exception e) {
+
+        }
     }
 }
